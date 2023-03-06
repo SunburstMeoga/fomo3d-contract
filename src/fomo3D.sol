fa9db5ec4e43a9c0e83cc5e1506891cc36760ab2
shang <shang_qd@qq.com>
2023-03-06 23:07:00 +0800

    基本测试

--- src/fomo3D.sol
+++ src/fomo3D.sol
@@ -17,69 +17,102 @@ library SafeMath {
 
 contract Fomo3D {
     using SafeMath for uint256;
-    uint256 public keyPrice; //六位小数
+    //uint256 public keyPrice; //六位小数
     uint256 public pool; //奖池
     uint256 public keyOwnPool; //key持有者的地址
     uint256 public platformPool; //平台
     uint256 public roundTime;
     address public winner;
-    uint256 public key_v;
     uint256 public eth_v;
     uint256 public nextRound;
+    uint256 public epoch;
+    uint256 public keysTotal;
 
-    // // 构造函数
+    struct Epoch {
+        uint256 keyOwnPool;
+        uint256 keysTotal;
+    }
+    mapping(uint256 => Epoch) epochs;
+
     constructor() {
-        // payable 表示合约地址可以接收转账
-        keyPrice = 1000000; // 获取转给合约地址的金额
+        initPriace();
+        epoch = 0;
     }
 
-    //判断是否为合约开发者
-    // modifier isHuman() {
-    //     require(msg.sender == owner, "Not Owner!");
-    //     _;
-    // }
-    //购买key，并返回购买的key的数量(向当前合约转入eth)
+    function newPlay() private {
+        initPriace();
+        epoch++;
+    }
 
     struct info {
         uint256 balance;
         uint8 team; //0.鱼 1.熊 2.蛇 3.牛
-        uint256 eth;
+        uint256 epoch;
     }
-    mapping(address => info) infos;
+
+    mapping(address => info) public infos;
 
     function buyKeys(uint256 value, uint8 team) public payable {
-        infos[msg.sender].balance = infos[msg.sender].balance.add(msg.value);
-        infos[msg.sender].team = team;
-        assert(value * keyPrice * 10**12 <= msg.value);
+        address addr = msg.sender;
+        uint256 eth_value = msg.value;
+        uint key_v = getKeys(eth_value);
+        infos[addr].balance = infos[addr].balance.add(value);
+        infos[addr].team = team;
+        assert(value <= key_v);
+        keysTotal += value;
+        updatePriace(eth_value);
         roundTime = block.timestamp;
-        winner = msg.sender;
-        if (infos[msg.sender].team == 0) {
-            pool = pool.add(value / 2); //50% 奖池
-            keyOwnPool = keyOwnPool.add((value * 30) / 100); //30% key持有者
-            platformPool = platformPool.add((value * 2) / 100); //2% 给平台
-        } else if (infos[msg.sender].team == 1) {
-            pool = pool.add((value * 43) / 100); //43%
-            keyOwnPool = keyOwnPool.add((value * 43) / 100); //30% key持有者
-            platformPool = platformPool.add((value * 2) / 100); //2% 给平台
-        } else if (infos[msg.sender].team == 2) {
-            pool = pool.add(value / 5); //20%
-            keyOwnPool = keyOwnPool.add(value / 10); //30% key持有者
-            platformPool = platformPool.add((value * 2) / 100); //2% 给平台
+        winner = addr;
+        
+        if (infos[addr].team == 0) {
+            pool = pool.add(eth_value / 2); //50% 奖池
+            keyOwnPool = keyOwnPool.add((eth_value * 30) / 100); //30% key持有者
+            platformPool = platformPool.add((eth_value * 2) / 100); //2% 给平台
+        } else if (infos[addr].team == 1) {
+            pool = pool.add((eth_value * 43) / 100); //43%
+            keyOwnPool = keyOwnPool.add((eth_value * 43) / 100); //30% key持有者
+            platformPool = platformPool.add((eth_value * 2) / 100); //2% 给平台
+        } else if (infos[addr].team == 2) {
+            pool = pool.add(eth_value / 5); //20%
+            keyOwnPool = keyOwnPool.add(eth_value / 10); //30% key持有者
+            platformPool = platformPool.add((eth_value * 2) / 100); //2% 给平台
         } else {
-            pool = pool.add((value * 35) / 100); //35%
-            keyOwnPool = keyOwnPool.add((value * 8) / 100); //30% key持有者
-            platformPool = platformPool.add((value * 2) / 100); //2% 给平台
+            pool = pool.add((eth_value * 35) / 100); //35%
+            keyOwnPool = keyOwnPool.add((eth_value * 8) / 100); //30% key持有者
+            platformPool = platformPool.add((eth_value * 2) / 100); //2% 给平台
         }
     }
 
-    function withdraw() external payable {
+    function withdraw1() external payable {
+        address addr = msg.sender;
         assert(
-            msg.sender == winner && roundTime + (3600 * 24) > block.timestamp
+            addr == winner && roundTime + (3600 * 24) <= block.timestamp
         );
-        if (infos[msg.sender].team == 0) {
+        
+        if (infos[addr].epoch == epoch) {
+            epochs[epoch] = Epoch({keyOwnPool:keyOwnPool,keysTotal:keysTotal});
+            payable(addr).transfer(keyOwnPool.mul(infos[addr].balance) / keysTotal);
+            keyOwnPool = 0;
+            keysTotal = 0;
+            newPlay();
+            infos[addr].epoch = epoch;
+            infos[addr].balance = 0;
+        }
+        /*
+        if (infos[addr].team == 0) {
             payable(winner).transfer((pool * 15) / 100);
         } else {
             payable(winner).transfer((pool * 48) / 100);
+        }*/
+    }
+
+    function withdraw2() external payable {
+        address addr = msg.sender;
+        if (infos[addr].epoch < epoch) {
+            Epoch memory obj = epochs[infos[addr].epoch];
+            payable(addr).transfer(obj.keyOwnPool.mul(infos[addr].balance) / obj.keysTotal);
+            infos[addr].epoch = epoch;
+            infos[addr].balance = 0;
         }
     }
 
@@ -98,12 +131,11 @@ contract Fomo3D {
     }
 
     function initPriace() private {
-        key_v = 1_000_000 ether;
         eth_v = 1 ether;
     }
 
-    function getKeys(uint256 v) private view returns (uint256) {
-        return (key_v * v) / eth_v;
+    function getKeys(uint256 v) public view returns (uint256) {
+        return (1_000_000 ether * v) / eth_v;
     }
 
     function updatePriace(uint256 v) private {
--- test/fomo.ts
+++ test/fomo.ts
@@ -1,6 +1,6 @@
-import { expect, use } from 'chai'
+import { assert, expect, use } from 'chai'
 import { Contract, constants } from 'ethers'
-import { MockProvider,deployContract, createFixtureLoader, solidity } from 'ethereum-waffle'
+import { MockProvider, deployContract, solidity } from 'ethereum-waffle'
 
 import Fomo3D from '../build/Fomo3D.json';
 
@@ -11,10 +11,10 @@ const overrides = {
 }
 
 describe('fomo3D test', () => {
-    let fomo3D : Contract
+    let fomo3D: Contract
 
     const provider = new MockProvider()
-    const [wallet, wallet1, wallet2, wallet2_new, wallet3, wallet4, wallet5] = provider.getWallets()
+    const [wallet, wallet1, wallet2, wallet3, wallet4, wallet5] = provider.getWallets()
 
     beforeEach(async () => {
         fomo3D = await deployContract(wallet, Fomo3D)
@@ -23,15 +23,37 @@ describe('fomo3D test', () => {
     it('用例1', async () => {
         describe('--------------------------------', () => {
             it('购买', async () => {
-                expect(fomo3D.address).to.eq('0xA193E42526F1FEA8C99AF609dcEabf30C1c29fAA')
+                const eth_v = constants.WeiPerEther
+                const keys = await fomo3D.getKeys(eth_v)
+                expect(keys).to.eq(constants.WeiPerEther.mul(1000000))
+                await fomo3D.buyKeys(keys, 1, { ...overrides, value: eth_v })
+                expect(await provider.getBalance(fomo3D.address)).to.eq(eth_v)
             })
 
             it('竞争', async () => {
-                expect(fomo3D.address).to.eq('0xA193E42526F1FEA8C99AF609dcEabf30C1c29fAA')
+                const eth_v = constants.WeiPerEther
+                const keys = await fomo3D.getKeys(eth_v)
+                await fomo3D.connect(wallet1).buyKeys(keys, 1, { ...overrides, value: eth_v })
+                expect(await provider.getBalance(fomo3D.address)).to.eq(eth_v.mul(2))
+                const balance0 = (await fomo3D.infos(wallet.address)).balance
+                const balance1 = (await fomo3D.infos(wallet1.address)).balance
+                expect(balance0).to.gt(balance1)
             })
-        
-            it('提现', async () => { 
-                expect(fomo3D.address).to.eq('0xA193E42526F1FEA8C99AF609dcEabf30C1c29fAA')
+
+            it('提现', async () => {
+                let timestamp = (await provider.getBlock('latest')).timestamp + 24 * 60 * 60
+                await provider.send('evm_setTime', [timestamp * 1000])
+                await provider.send('evm_mine', [timestamp])
+                await expect(fomo3D.withdraw1()).to.be.reverted
+                let old_v = await provider.getBalance(wallet1.address)
+                await fomo3D.connect(wallet1).withdraw1()
+                let new_v = await provider.getBalance(wallet1.address)
+                assert(new_v > old_v)
+                let ret = await fomo3D.infos(wallet.address)
+                expect(ret.epoch).to.eq(0)
+                await fomo3D.withdraw2()
+                ret = await fomo3D.infos(wallet.address)
+                expect(ret.epoch).to.eq(1)
             })
         })
     })
