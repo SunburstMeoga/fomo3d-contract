c44dab94166986646d1859e12c626bdd0d809a3b
shang <shang_qd@qq.com>
2023-06-08 20:04:25 +0800

    OK

--- 分收益.py
+++ 分收益.py
@@ -18,9 +18,15 @@ def Buy(addr):
     addrs.append(addr)
     
 def 分收益():
+    s1 = 0
+    s2 = 0
     for index, addr in enumerate(addrs):
         v = (HAH_G * addr["keyNumber"] / KeysNumber_G) - addr["mask"]
+        s1 += v
+        s2 += addr["HAH"]
         print(f"{index}收益:{v}")
+    assert(s1 == s2)
+    print(f'全部收益:{s1}')
 
 def testA():       
     addr0 = {"keyNumber":1,"HAH":1,"mask":0}
@@ -45,4 +51,4 @@ def testB():
     分收益()
     #print(addrs)
 
-testB()
+testA()
